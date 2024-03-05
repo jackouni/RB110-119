ac0b362e5c973cd8e654500bf681e699260387f0
Jack Sebben <jacksebben11@hotmail.com>
2024-03-05 13:05:22 -0500

    Finished Launch School's walkthrough. I definitely liked their method for this game a lot better. As opposed to using a 2D array to represent the board like I did they used a hash with keys representing the squares and values representing the marker in the squares on the board. To note: my game got complicated quickly with all the extra validation I tried doing. I need to focus on the main functionality first and foremost.

--- lesson_3/tictactoe_walkthrough.rb
+++ lesson_3/tictactoe_walkthrough.rb
@@ -9,6 +9,8 @@ def prompt(message)
 end 
 
 def display_board(board)
+  system 'clear'
+  puts "You are #{USER_MARKER}'s | Computer is #{COMP_MARKER}'s"
   puts "      |      |     "
   puts "  #{board[1]}   |  #{board[2]}   |  #{board[3]}  "
   puts "      |      |     "
@@ -20,6 +22,7 @@ def display_board(board)
   puts "      |      |     "
   puts "  #{board[7]}   |  #{board[8]}   |  #{board[9]}  "
   puts "      |      |     "
+  puts " "
 end 
 
 def intialize_board 
@@ -29,7 +32,6 @@ def intialize_board
 end 
 
 def empty_squares(board)
-  binding.pry
   board.keys.select {|num| board[num] == EMPTY_MARKER}
 end
 
@@ -47,9 +49,73 @@ def player_places_piece!(board)
   board[square] = USER_MARKER 
 end 
 
-board = intialize_board
-display_board(board)
+def comp_places_piece!(board)
+  square = empty_squares(board).sample
+  board[square] = COMP_MARKER
+end 
 
-player_places_piece!(board)
-puts board.inspect
-display_board(board)
+def board_full?(board)
+  empty_squares(board).empty?
+end 
+
+def someone_won?(board)
+  !!detect_winner(board)
+end
+
+def detect_winner(board)
+  winning_lines = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ]
+
+  winner = nil
+
+  winning_lines.each do |line|
+    if line.all? { |square| board[square] == USER_MARKER }
+      winner = 'User' 
+      break
+    end 
+  end 
+
+  winning_lines.each do |line|
+    if line.all? { |square| board[square] == COMP_MARKER }
+      winner = 'Computer' 
+      break
+    end 
+  end 
+
+  winner
+end
+
+loop do # Main Game Loop
+  board = intialize_board
+
+  loop do # Round Loop
+    display_board(board)
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    comp_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end 
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end 
+
+  prompt "Play again? (y/n):"
+  answer = gets.chomp
+  break if answer == 'n' || answer == 'N'
+end 
+
+prompt "Thanks for playing TicTacToe, goodbye!"
