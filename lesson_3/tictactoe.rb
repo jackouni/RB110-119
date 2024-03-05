560d136815fc5f3ea138b5611f83e7b12b650248
Jack Sebben <jacksebben11@hotmail.com>
2024-03-04 20:46:56 -0500

    This is my version of the game. It's a little rough, but you can play a game. It often bugs out if you attempt to play again though. I think I'll do the Launch School walkthrough/tutorial and see how it compares and where I've messed up.

--- lesson_3/tictactoe.rb
+++ lesson_3/tictactoe.rb
@@ -1,5 +1,41 @@
 require 'pry'
 
+WIN_POSITIONS = [
+  [[0,0], [0,1], [0,2]],
+  [[1,0], [1,1], [1,2]],
+  [[2,0], [2,1], [2,2]],
+  [[0,0], [1,0], [2,0]],
+  [[0,1], [1,1], [2,1]],
+  [[0,2], [1,2], [2,2]],
+  [[0,0], [1,1], [2,2]],
+  [[0,2], [1,1], [2,0]]
+]
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def new_game
+  prompt "Awesome, let's start a new game!"
+end
+
+def end_game
+  prompt "No worries, goodbye."
+end
+
+def game_message(outcome)
+  case outcome
+  when 'user'
+    prompt "Congratulations, you won!"
+  when 'comp'
+    prompt "Shucks, looks like the computer beat you this time."
+  when 'tie' 
+    prompt "It's a tie game."
+  else
+    puts "Error in game message sequence."
+  end 
+end 
+
 def valid_input?(input)
   if input != input.to_i.to_s
     false
@@ -17,7 +53,7 @@ def valid_position?(coordinate, board)
   board[row][col] == '*'
 end 
 
-def win?(board, marker)
+def win?(board)
   is_win = false
   WIN_POSITIONS.each do |coordinates|
     mark_counter = 0 
@@ -25,7 +61,7 @@ def win?(board, marker)
       row = coordinate[0]
       col = coordinate[1]
       
-      mark_counter += 1 if board[row][col] == marker 
+      mark_counter += 1 if board[row][col] != '*' 
     end 
 
     if mark_counter == 3
@@ -37,19 +73,38 @@ def win?(board, marker)
   is_win 
 end 
 
+def board_full?(board)
+  is_empty = true
+
+  board.each do |row|
+    row.each do |column|
+      if column == '*'
+        is_empty = false
+        break
+      end
+    end 
+  end 
+
+  is_empty
+end
+
 def display_winner(winner)
   case winner
   when 'user'
-    puts "Congratulations! You won!!"
+    prompt "Congratulations! You won!!"
   when 'comp'
-    puts "Shucks. Computer beat you this time!"
+    prompt "Shucks. Computer beat you this time!"
   else
-    puts 'Error'
+    prompt 'Error'
   end 
 end 
 
+def display_tie
+  prompt "Tie game!"
+end 
+
 def users_move(board)
-  puts "Place your marker!"
+  prompt "Place your marker!"
   user_row = nil
   user_col = nil
   coordinate = []
@@ -57,17 +112,17 @@ def users_move(board)
   loop do   # Validating marker position
 
     loop do # Getting the row position
-      puts "Pick a row number between 1 and 3:"
+      prompt "Pick a row number between 1 and 3:"
       user_row = gets.chomp
       break if valid_input?(user_row)
-      puts "Please enter a valid input!!"
+      prompt "Please enter a valid input!!"
     end 
 
     loop do # Getting the column position
-      puts "Pick a column number between 1 and 3:"
+      prompt "Pick a column number between 1 and 3:"
       user_col = gets.chomp
       break if valid_input?(user_col)
-      puts "Please enter a valid input!!"
+      prompt "Please enter a valid input!!"
     end
 
     row = user_row.to_i - 1
@@ -75,8 +130,8 @@ def users_move(board)
     coordinate = [row, col]
 
     break if valid_position?(coordinate, board)
-    puts "Sorry, that position has already been marked!!"
-    puts "Please enter your coordinates again!!"
+    prompt "Sorry, that position has already been marked!!"
+    prompt "Please enter your coordinates again!!"
   end 
 
   coordinate
@@ -95,6 +150,25 @@ def computers_move(board)
   coordinate
 end
 
+def play_again?()
+  play_again = true
+  loop do
+    prompt "Would you like to play again? ('y' or 'n'):"
+    user_input = gets.chomp
+
+    if user_input == 'n'
+      play_again = false
+      break
+    elsif user_input == 'y'
+      break
+    else 
+      prompt "Please enter a valid input. Either 'y' for 'yes' or 'n' for 'no'."
+    end 
+  end 
+
+  play_again
+end 
+
 def display_board(board)
   puts "CURRENT BOARD"
   puts "-------------"
@@ -104,20 +178,26 @@ def display_board(board)
   puts "-------------"
 end 
 
-def update_board(coordinate, whos_turn, board)
+def update_board(coordinate, marker, board)
   row = coordinate[0]
   col = coordinate[1]
 
-  if whos_turn == 'user'
+  if marker == 'X'
     board[row][col] = 'X'
   else 
     board[row][col] = 'O'
   end 
 end 
 
-# --------------------------
+def reset_board(board)
+  board.map! do |row|
+    row.map! {|col| '*'}
+  end 
+end 
 
-puts "Welcome to tictactoe!"
+# -----------------------------
+
+prompt "Welcome to tictactoe!"
 
 board = [
   ['*', '*', '*'],
@@ -125,31 +205,65 @@ board = [
   ['*', '*', '*']
 ]
 
-WIN_POSITIONS = [
-  [[0,0], [0,1], [0,2]],
-  [[1,0], [1,1], [1,2]],
-  [[2,0], [2,1], [2,2]],
-  [[0,0], [1,0], [2,0]],
-  [[0,1], [1,1], [2,1]],
-  [[0,2], [1,2], [2,2]],
-  [[0,1], [1,1], [2,2]],
-  [[0,2], [1,1], [2,0]]
-]
-
+outcome = nil
+binding.pry
 loop do
-  user_mark = users_move(board)
-  update_board(user_mark, 'user', board)
-  if win?(board, 'X')
-    display_winner('user')
-    break
+  user_mark = users_move(board)        # User makes a move
+  update_board(user_mark, 'X', board)
+
+  if win?(board)                       # Player wins
+    outcome = 'user'
+    display_board(board)
+    game_message(outcome)
+
+    if play_again?()
+      reset_board(board)
+      outcome = nil
+      new_game()
+      next
+    else
+      end_game()
+      break
+    end 
   end 
-  
-  comp_mark = computers_move(board)
-  update_board(comp_mark, 'computer', board)
-  if win?(board, 'O')
-    display_winner('comp')
-    break
-  end
-  display_board(board)
-end 
+
+  comp_mark = computers_move(board)   # Comp makes a move
+  update_board(comp_mark, 'O', board)
+
+  if win?(board)                      # Comp wins
+    outcome = 'comp'
+    display_board(board)
+    game_message(outcome)
+
+    if play_again?()
+      reset_board(board)
+      outcome = nil
+      new_game()
+      next
+    else
+      end_game()
+      break
+    end 
+
+  elsif board_full?(board)             # No wins and board is full
+    outcome = 'tie'
+    display_board(board)
+    game_message(outcome)
+
+    if play_again?()
+      reset_board(board)
+      outcome = nil
+      new_game()
+      next
+    else
+      end_game()
+      break
+    end  
+    
+  else                                 # No wins, next round
+    display_board(board)
+    next
+  end  
+
+end  
 
