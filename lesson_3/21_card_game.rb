6302329546ca5bcc61f984f2ab509e90001143f8
Jack Sebben <jacksebben11@hotmail.com>
2024-03-07 07:59:58 -0500

    Finished my version of 21, now I'm going to compare against Launch School's solution. There is no walkthrough, so I think I will just do my own review. My game is fully functional.

--- lesson_3/21_card_game.rb
+++ lesson_3/21_card_game.rb
@@ -18,9 +18,7 @@ def initialize_deck
   deck.flatten
 end
 
-#########
-
-def deal_card(deck, player)
+def deal_card!(deck, player)
   card_dealt = deck.sample
 
   player[:hand] << card_dealt
@@ -44,12 +42,19 @@ def joinor(arr, delimiter=', ', word='or')
   end 
 end
 
+def reveal_dealer_hand(dealers_hand)
+  puts "------------------------------------------------"
+  puts "Dealer has: #{joinor(dealers_hand, ', ', 'and')}"
+end
+
 def display_dealer_hand(dealers_hand)
+  puts "------------------------------------------------"
   puts "Dealer has: #{dealers_hand[0]} and unknown card"
 end
 
 def display_user_hand(users_hand)
   puts "You have: #{joinor(users_hand, ', ', 'and')}"
+  puts "------------------------------------------------"
 end
 
 def get_ace_value(players_total)
@@ -66,15 +71,77 @@ def get_card_value(player, card)
   end
 end 
 
-def updated_total(player)
+def update_score(player)
   player[:score] = 0 
   player[:hand].each { |card| player[:score] += get_card_value(player, card) }
 end 
 
-###
+def bust?(player)
+  player[:score] > 21 ? true : false 
+end
+
+def detect_winner(user, dealer)
+  return 'user' if bust?(dealer)
+  return 'dealer' if bust?(user)
+  
+  if user[:score] > dealer[:score]
+    'user'
+  elsif user[:score] < dealer[:score]
+    'dealer'
+  else
+    'tie'
+  end 
+end
+
+def user_turn(user, dealer, deck) # User turn: 'hit' or 'stay'
+  loop do
+    update_score(user)
+    display_dealer_hand(dealer[:hand])
+    display_user_hand(user[:hand])
+    prompt "| Your points: #{user[:score]} |"
+    
+    if bust?(user) 
+      system 'clear'
+      prompt "You busted!"
+      break
+    end
+
+    prompt "Do you want to \"hit\" or \"stay\"?"
+    user_choice = gets.chomp
+    if user_choice == 'hit'
+      system 'clear'
+      deal_card!(deck, user)
+    elsif user_choice == 'stay'
+      system 'clear'
+      break
+    else
+      prompt "Please input a valid answer!! (Type: 'hit' or 'stay')"
+      system 'clear'
+    end 
+  end 
+end 
+
+def dealer_turn(dealer, deck)
+  loop do
+    update_score(dealer)
+
+    if bust?(dealer) 
+      system 'clear'
+      prompt "Dealer busted!"
+      break
+    end
+
+    if dealer[:score] < 17
+      deal_card!(deck, dealer)
+    else
+      break
+    end 
+  end 
+end
+
+### GAME BEGIN
 
 deck = initialize_deck()
-winner = nil
 
 user = {
   hand: [],
@@ -87,31 +154,25 @@ dealer = {
 }
 
 2.times do
-  deal_card(deck, user)
-  deal_card(deck, dealer)
+  deal_card!(deck, user)
+  deal_card!(deck, dealer)
 end 
 
-loop do 
-  display_dealer_hand(dealer[:hand])
-  display_user_hand(user[:hand])
-  updated_total(user)
+user_turn(user, dealer, deck)
+dealer_turn(dealer, deck) unless bust?(user)
+update_score(dealer)
 
-  break if user[:score] > 21 
+reveal_dealer_hand(dealer[:hand])
+display_user_hand(user[:hand])
 
-  prompt "Your points: #{user[:score]}"
-  prompt "Do you want to \"hit\" or \"stay\"?"
-  user_choice = gets.chomp
-  if user_choice == 'hit'
-    deal_card(deck, user)
-  elsif user_choice == 'stay'
-    break
-  else
-    prompt "Please input a valid answer!! (Type: 'hit' or 'stay')"
-    puts ""
-  end 
-end
+prompt "| Your points: #{user[:score]} | Dealer's points: #{dealer[:score]}"
 
-if user[:score] > 21
-  # winner = 'dealer'
-  # break main game loop??
+winner = detect_winner(user, dealer)
+
+if winner == 'user'
+  prompt "Congrats, you beat the dealer!"
+elsif winner == 'dealer'
+  prompt "The dealer beat you this time!"
+else
+  prompt "Tie game."
 end
