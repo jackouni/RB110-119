41d94b4a045304d7231ade7a8aa26fcb939527a1
Jack Sebben <jacksebben11@hotmail.com>
2024-03-07 19:55:38 -0500

    Game is finished (for now). Added a feature that allows the hand of a player to be reset. This is done to reset the hands if the user decides to play another game. Added a feature that also keeps track of score. I did this by housing the score in the player's hashes and then referencing those hash values when necessary, along with a method that can increment those values.

--- lesson_3/21_card_game.rb
+++ lesson_3/21_card_game.rb
@@ -1,6 +1,8 @@
 TEN_POINTS = ['queen', 'king', 'jack']
 FACE_VALUES = (2..10)
 
+### DISPLAYS & OUTPUTS:
+
 def prompt(msg)
   puts "=> #{msg}"
 end
@@ -16,8 +18,31 @@ def joinor(arr, delimiter=', ', word='or')
   end
 end
 
+def reveal_dealer_hand(dealers_hand)
+  puts "------------------------------------------------"
+  puts "Dealer has: #{joinor(dealers_hand, ', ', 'and')}"
+end
+
+def display_dealer_hand(dealers_hand)
+  puts "------------------------------------------------"
+  puts "Dealer has: #{dealers_hand[0]} and unknown card"
+end
+
+def display_user_hand(users_hand)
+  puts "You have: #{joinor(users_hand, ', ', 'and')}"
+  puts "------------------------------------------------"
+end
+
+def display_scores(user, dealer)
+  puts "_____________________"
+  puts "|USER   V.S   DEALER|"
+  puts "  #{user[:score]}              #{dealer[:score]} "
+end
+
+### GAME MECHANICS:
+
 def create_player
-  { hand: [], score: 0 }
+  { hand: [], points: 0, score: 0 }
 end
 
 def initialize_deck
@@ -46,21 +71,6 @@ def deal_card!(deck, player)
   end
 end
 
-def reveal_dealer_hand(dealers_hand)
-  puts "------------------------------------------------"
-  puts "Dealer has: #{joinor(dealers_hand, ', ', 'and')}"
-end
-
-def display_dealer_hand(dealers_hand)
-  puts "------------------------------------------------"
-  puts "Dealer has: #{dealers_hand[0]} and unknown card"
-end
-
-def display_user_hand(users_hand)
-  puts "You have: #{joinor(users_hand, ', ', 'and')}"
-  puts "------------------------------------------------"
-end
-
 def get_ace_value(players_total)
   players_total <= 10 ? 11 : 1
 end
@@ -71,39 +81,72 @@ def get_card_value(player, card)
   elsif TEN_POINTS.include? card
     10
   else
-    get_ace_value(player[:score])
+    get_ace_value(player[:points])
   end
 end
 
-def update_score!(player)
-  player[:score] = 0
-  player[:hand].each { |card| player[:score] += get_card_value(player, card) }
+def update_points!(player)
+  player[:points] = 0
+  player[:hand].each { |card| player[:points] += get_card_value(player, card) }
 end
 
 def bust?(player)
-  player[:score] > 21
+  player[:points] > 21
 end
 
 def detect_winner(user, dealer)
   return 'user' if bust?(dealer)
   return 'dealer' if bust?(user)
 
-  if user[:score] > dealer[:score]
+  if user[:points] > dealer[:points]
     'user'
-  elsif user[:score] < dealer[:score]
+  elsif user[:points] < dealer[:points]
     'dealer'
   else
     'tie'
   end
 end
 
-def user_turn(user, dealer, deck) # User turn: 'hit' or 'stay'
+def play_again?
+  user_plays_again = false
+
   loop do
-    update_score!(user)
+    prompt "Would you like to play again? ('y' / 'n'):"
+    user_choice = gets.chomp.downcase
+
+    case user_choice
+    when 'y'
+      user_plays_again = true
+      break
+    when 'n'
+      break
+    else
+      system 'clear'
+      prompt "Please select a valid input!!"
+    end
+  end
+
+  user_plays_again
+end
+
+def increment_score(player)
+  player[:score] += 1
+end
+
+def reset_hand(player)
+  player[:hand] = []
+end
+
+### USER & DEALER TURNS:
+
+def user_turn(user, dealer, deck)
+  loop do
+    update_points!(user)
+    display_scores(user, dealer)
     display_dealer_hand(dealer[:hand])
     display_user_hand(user[:hand])
 
-    prompt "| Your points: #{user[:score]} |"
+    prompt "| Your points: #{user[:points]} |"
 
     if bust?(user)
       system 'clear'
@@ -121,15 +164,15 @@ def user_turn(user, dealer, deck) # User turn: 'hit' or 'stay'
       system 'clear'
       break
     else
-      prompt "Please input a valid answer!! (Type: 'hit' or 'stay')"
       system 'clear'
+      prompt "Please input a valid answer!! (Type: 'hit' or 'stay')"
     end
   end
 end
 
 def dealer_turn(dealer, deck)
   loop do
-    update_score!(dealer)
+    update_points!(dealer)
 
     if bust?(dealer)
       system 'clear'
@@ -137,7 +180,7 @@ def dealer_turn(dealer, deck)
       break
     end
 
-    if dealer[:score] < 17
+    if dealer[:points] < 17
       deal_card!(deck, dealer)
     else
       break
@@ -145,33 +188,53 @@ def dealer_turn(dealer, deck)
   end
 end
 
-### GAME START
+### GAMEPLAY
 
 deck = initialize_deck
 
 user = create_player
 dealer = create_player
 
-2.times do
-  deal_card!(deck, user)
-  deal_card!(deck, dealer)
-end
+loop do
+  2.times do
+    deal_card!(deck, user)
+    deal_card!(deck, dealer)
+  end
 
-user_turn(user, dealer, deck)
-dealer_turn(dealer, deck) unless bust?(user)
+  user_turn(user, dealer, deck)
+  dealer_turn(dealer, deck) unless bust?(user)
 
-update_score!(dealer)
-reveal_dealer_hand(dealer[:hand])
-display_user_hand(user[:hand])
+  update_points!(dealer)
+  reveal_dealer_hand(dealer[:hand])
+  display_user_hand(user[:hand])
 
-prompt "| Your points: #{user[:score]} | Dealer's points: #{dealer[:score]}"
+  ### ROUND CONCLUDES:
 
-winner = detect_winner(user, dealer)
+  prompt "| Your points: #{user[:points]} | Dealer's points: #{dealer[:points]}"
 
-if winner == 'user'
-  prompt "Congrats, you beat the dealer!"
-elsif winner == 'dealer'
-  prompt "The dealer beat you this time!"
-else
-  prompt "Tie game."
+  winner = detect_winner(user, dealer)
+
+  if winner == 'user'
+    increment_score(user)
+    prompt "Congrats, you beat the dealer!"
+  elsif winner == 'dealer'
+    increment_score(dealer)
+    prompt "The dealer beat you this time!"
+  else
+    prompt "Tie game."
+  end
+
+  if play_again?
+    reset_hand(user)
+    reset_hand(dealer)
+    deck = initialize_deck
+
+    system 'clear'
+    prompt "New game started!"
+    sleep(1.5)
+    system 'clear'
+  else
+    prompt "Thanks for playing. Goodbye."
+    break
+  end
 end
